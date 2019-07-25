64eb28216295019d7eb011108850d95fa140ffda
Paolo Rosso <paolo@i2k.it>
2019-07-25 10:00:01 +0200

    init

--- LOAD.cs
+++ LOAD.cs
@@ -7,7 +7,7 @@ namespace Api
 {
     public class LOAD
     {
-        public const string ConnDb = @"Server=pcpaolo\SQLEXPRESS2017;Database=Test;User ID = sa; Password=sqladmin;Trusted_Connection=False;multipleactiveresultsets=True;";
+        public const string ConnDb = @"Server=localhost\SQLEXPRESS2017;Database=Test;User ID = sa; Password=sqladmin;Trusted_Connection=False;multipleactiveresultsets=True;";
         public const string Chiave = "******************";   // Secret key
     }
 }
